891bf50310cc722e8d0e45490532f46ecdba4f96
Florent Guillaume <fg@nuxeo.com>
2012-01-05 16:16:52 +0100

    tabs -> whitespace

--- simplifyjavadoc.py
+++ simplifyjavadoc.py
@@ -13,49 +13,49 @@ def gen(filename):
     step = 0
     lines = []
     for line in file:
-	line = line.rstrip()
-	if step == 0:
-	    if line == '<!-- ======== START OF CLASS DATA ======== -->':
-	      step = 1
-	    continue
-	if step == 1:
-	    if line == '<HR>':
-	      step = 2
-	    continue
-	if step == 2:
-	    if line == '</PRE>':
-	      step = 3
-	    continue
+        line = line.rstrip()
+        if step == 0:
+            if line == '<!-- ======== START OF CLASS DATA ======== -->':
+              step = 1
+            continue
+        if step == 1:
+            if line == '<HR>':
+              step = 2
+            continue
+        if step == 2:
+            if line == '</PRE>':
+              step = 3
+            continue
 
-	if line.endswith('>Serialized Form</A></DL>'):
+        if line.endswith('>Serialized Form</A></DL>'):
             line = '</DL>'
-	elif line.startswith('<DT><B>See Also:</B>'):
-	    continue
+        elif line.startswith('<DT><B>See Also:</B>'):
+            continue
 
-	if line == '<DT><B>Author:</B></DT>':
-	    step = 4
-	    continue
-	if step == 4:
-	    step = 3 # skip author
-	    continue
-	
-	if line == '<HR>':
-	    break
+        if line == '<DT><B>Author:</B></DT>':
+            step = 4
+            continue
+        if step == 4:
+            step = 3 # skip author
+            continue
+        
+        if line == '<HR>':
+            break
 
-	lines.append(line)
+        lines.append(line)
 
     file.close()
 
     # strip some stuff at beginning and end
 
     while len(lines) and (lines[0] == '' or lines[0] == '<P>'):
-	del lines[0]
+        del lines[0]
 
     if len(lines) >= 2 and lines[len(lines)-2] == '<DL>' and lines[len(lines)-1] == '</DL>':
-	del lines[len(lines)-2:]
+        del lines[len(lines)-2:]
 
     while len(lines) and (lines[len(lines)-1] == '' or lines[len(lines)-1] == '<P>' or lines[len(lines)-1] == '<DL>'):
-	del lines[len(lines)-1]
+        del lines[len(lines)-1]
 
     # output result
 
@@ -63,7 +63,7 @@ def gen(filename):
     print "Generating " + newfilename
     out = open(newfilename, 'w+')
     for line in lines:
-	print >> out, line
+        print >> out, line
     out.close()
 
 
